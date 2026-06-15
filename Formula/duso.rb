886d183978164bfcf6a3b2e662851e8c765cf105
Dave Balmer <dbalmerjr@gmail.com>
2026-06-15 13:43:31 -0700

    fix: duso 1.4.3

--- Formula/duso.rb
+++ Formula/duso.rb
@@ -5,23 +5,23 @@ class Duso < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/duso-org/duso/releases/download/v1.0.10-402/duso-macos-intel.zip"
-      sha256 "6b346229753991703275fa2f5e0342da2cd6029478cd19151a37fc137213caa5"
+      url "https://github.com/duso-org/duso/releases/download/v1.4.3-462/duso-macos-intel.zip"
+      sha256 "881a3aa2a4748a37caed78f5ea17d3444406be23a6ca0e98a7dc431af8f31628"
     end
     on_arm do
-      url "https://github.com/duso-org/duso/releases/download/v1.0.10-402/duso-macos-silicon.zip"
-      sha256 "e701c0dd7d8ca101d521e134d70ffe677857e16abf187659601b35cd96bca9f3"
+      url "https://github.com/duso-org/duso/releases/download/v1.4.3-462/duso-macos-silicon.zip"
+      sha256 "c8af34c5da0a4874b409d1c410e40a5676714bed1f3cdef7517767fd8f7483c1"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/duso-org/duso/releases/download/v1.0.10-402/duso-linux-amd64.tar.gz"
-      sha256 "45f4fb66a67351b860a62a32f260756fcdad0defe6a64928cbc7c660b9880be6"
+      url "https://github.com/duso-org/duso/releases/download/v1.4.3-462/duso-linux-amd64.tar.gz"
+      sha256 "082296b1f311405727502ec4fc7c96760d7ea0278f781e7085e88fc6a98814a5"
     end
     on_arm do
-      url "https://github.com/duso-org/duso/releases/download/v1.0.10-402/duso-linux-arm64.tar.gz"
-      sha256 "eccab386e711f59ce1c775bdc6d86009f1c547be8a0977fcd9b2b981f69643da"
+      url "https://github.com/duso-org/duso/releases/download/v1.4.3-462/duso-linux-arm64.tar.gz"
+      sha256 "a1ed67b85b61f60986bdb1a35de632105aeedaa16651d25985b571c510a58fa2"
     end
   end
 
